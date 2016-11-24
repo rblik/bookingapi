86a23e3a1e9c72504b0f209742b7abd357bbd209
Renat Blik <rblick@bk.ru>
2016-11-24 14:31:04 +0200

    restaurant_repo_1

--- src/test/java/isr/ek0/orderapi/service/UserServiceTest.java
+++ src/test/java/isr/ek0/orderapi/service/UserServiceTest.java
@@ -1,22 +1,22 @@
-package isr.ek0.orderapi.repo;
+package isr.ek0.orderapi.service;
 
 import isr.ek0.orderapi.Application;
+import isr.ek0.orderapi.model.Meal;
+import isr.ek0.orderapi.model.Restaurant;
 import isr.ek0.orderapi.model.User;
-import isr.ek0.orderapi.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static isr.ek0.orderapi.util.UsersUtil.ADMIN;
-import static isr.ek0.orderapi.util.UsersUtil.TEST_USERS;
-import static isr.ek0.orderapi.util.UsersUtil.USER_1;
+import static isr.ek0.orderapi.util.UsersUtil.*;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-
 
 @RunWith(SpringRunner.class)
 @SpringApplicationConfiguration({Application.class})
@@ -29,7 +29,8 @@ public class UserServiceTest {
 
     @Before
     public void init() {
-        template.dropCollection(User.class);
+        template.dropCollection("users");
+        template.dropCollection("restaurants");
         template.insertAll(TEST_USERS);
     }
 
@@ -38,5 +39,11 @@ public class UserServiceTest {
         User userFound = service.get(ADMIN.getEmail());
         assertEquals(ADMIN, userFound);
         assertNotEquals(USER_1, userFound);
+    }
+
+    @Test
+    public void testSaveRestaurant() {
+        Restaurant restaurant = new Restaurant("My Restaurant", new GeoJsonPoint(10, -40), asList(new Meal("Porridge", 30, 10)));
+        service.addRestaurant(ADMIN.getEmail(), restaurant);
     }
 }
